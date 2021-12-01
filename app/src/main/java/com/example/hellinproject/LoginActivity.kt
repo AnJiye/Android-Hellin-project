ee5b264a7c9f750056573e9c2aef90d14ab92ead
AnJiye <68097036+AnJiye@users.noreply.github.com>
2021-12-01 23:35:53 +0900

    Google Login

--- app/src/main/java/com/example/hellinproject/LoginActivity.kt
+++ app/src/main/java/com/example/hellinproject/LoginActivity.kt
@@ -1,11 +1,85 @@
 package com.example.hellinproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    // firebase 인증을 위한 변수
+    var auth : FirebaseAuth? = null
+    // 구글 로그인 연동에 필요한 변수
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // 초기화
+        auth = FirebaseAuth.getInstance()
+
+        // 구글 로그인 버튼
+        google_sign_in_button.setOnClickListener {
+            //First step
+            googleLogin()
+        }
+        // 사용자 ID와 프로필 정보를 요청하기 위해 gso를 인자로 전달해서 GoogleSignInClient 객체 생성
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestIdToken("353497376762-p5sgflf136jnhf2cu39bp4ajjri0o8vl.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            // result가 성공했을 때 이 값을 firebase에 넘겨주기
+            if (result!!.isSuccess) {
+                var account = result.signInAccount
+                // Second step
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                task ->
+                if(task.isSuccessful) {
+                    // Login, 아이디와 패스워드가 맞았을 때
+                    moveMainPage(task.result?.user)
+                } else {
+                    // Show the error message, 아이디와 패스워드가 틀렸을 때
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    // 로그인이 성공하면 다음 페이지로 넘어가는 함수
+    fun moveMainPage(user : FirebaseUser?) {
+        // 파이어베이스 유저 상태가 있을 경우 다음 페이지로 넘어갈 수 있음
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
