88e55fe2f227ab9c55c59b72dcd35ff51b3495b7
AnJiye <oan215@naver.com>
2021-12-05 19:47:20 +0900

    survey page UI

--- app/src/main/java/com/example/hellinproject/ProfileSettingActivity.kt
+++ app/src/main/java/com/example/hellinproject/ProfileSettingActivity.kt
@@ -1,11 +1,19 @@
 package com.example.hellinproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.hellinproject.camera.LaunchActivity
+import kotlinx.android.synthetic.main.profile_setting.*
 
 class ProfileSettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile_setting)
+
+        profile_next_btn.setOnClickListener {
+            val intent = Intent(this, SurveyActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/hellinproject/SurveyActivity.kt
+++ app/src/main/java/com/example/hellinproject/SurveyActivity.kt
@@ -1,11 +1,18 @@
 package com.example.hellinproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_survey.*
 
 class SurveyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_survey)
+
+        survey_next_btn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
