0d97e03a4659a93288762ec7fd024f746b5a051e
AnJiye <68097036+AnJiye@users.noreply.github.com>
2021-12-07 17:34:55 +0900

    Modify posenet UI

--- app/src/main/java/com/example/hellinproject/ExplainPlankActivity.kt
+++ app/src/main/java/com/example/hellinproject/ExplainPlankActivity.kt
@@ -3,7 +3,6 @@ package com.example.hellinproject
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.hellinproject.camera.LaunchActivity
 import com.example.hellinproject.posenet.CameraActivity
 import kotlinx.android.synthetic.main.explain_plank.*
 
--- app/src/main/java/com/example/hellinproject/posenet/CameraActivity.kt
+++ app/src/main/java/com/example/hellinproject/posenet/CameraActivity.kt
@@ -25,7 +25,7 @@ class CameraActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.tfe_pn_activity_camera)
+    setContentView(R.layout.activity_camera)
     window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     savedInstanceState ?: supportFragmentManager.beginTransaction()
       .replace(R.id.container, PosenetActivity())
