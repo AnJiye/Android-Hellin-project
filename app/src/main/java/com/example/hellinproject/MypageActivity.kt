d18103ab0c146c1ba1e2785cefc06d66c31d3211
AnJiye <68097036+AnJiye@users.noreply.github.com>
2021-12-10 17:26:04 +0900

    setting page UI

--- app/src/main/java/com/example/hellinproject/MypageActivity.kt
+++ app/src/main/java/com/example/hellinproject/MypageActivity.kt
@@ -1,11 +1,18 @@
 package com.example.hellinproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_mypage.*
 
 class MypageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mypage)
+
+        setting_btn.setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
