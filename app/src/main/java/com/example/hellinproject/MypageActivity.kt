fd4e7aa274583e7a7efe8287ac5abbc56edb1ef4
AnJiye <68097036+AnJiye@users.noreply.github.com>
2021-12-10 16:39:20 +0900

    mypage UI

--- app/src/main/java/com/example/hellinproject/MypageActivity.kt
+++ app/src/main/java/com/example/hellinproject/MypageActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MypageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.my_page)
+        setContentView(R.layout.activity_mypage)
     }
 }
