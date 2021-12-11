afcc61db0467d12ba72d831d818d5202d7775a20
AnJiye <68097036+AnJiye@users.noreply.github.com>
2021-12-11 12:37:50 +0900

    Use Firebase Storage to save profile image

--- app/src/main/java/com/example/hellinproject/SettingActivity.kt
+++ app/src/main/java/com/example/hellinproject/SettingActivity.kt
@@ -10,20 +10,29 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_setting.*
+import kotlinx.android.synthetic.main.profile_setting.*
+import java.text.SimpleDateFormat
+import java.util.*
 
+// !! 앨범에서 사진 선택 안했을 시 FileNotFoundException 처리해야함
 class SettingActivity : AppCompatActivity() {
     var PICK_IMAGE_FROM_ALBUM = 0
     var photoUri : Any? = null
     var uid = FirebaseAuth.getInstance().currentUser?.uid
     var database : FirebaseDatabase? = null
+    var storage : FirebaseStorage? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
+        storage = FirebaseStorage.getInstance()
         database = FirebaseDatabase.getInstance()
         var databaseRef : DatabaseReference = database!!.reference
 
@@ -38,20 +47,15 @@ class SettingActivity : AppCompatActivity() {
         }
 
         setting_back_btn.setOnClickListener {
-            var nickname = setting_nickname_input.text.toString()
-            var height = Integer.parseInt(setting_height_input.text.toString())
-            var weight = Integer.parseInt(setting_weight_input.text.toString())
-            databaseRef.child("users").child(uid!!).child("nickname").setValue(nickname)
-            databaseRef.child("users").child(uid!!).child("height").setValue(height)
-            databaseRef.child("users").child(uid!!).child("weight").setValue(weight)
-            databaseRef.child("users").child(uid!!).child("userProfile").setValue(photoUri.toString())
-
+            profileUpload()
             val intent = Intent(this, MypageActivity::class.java)
             startActivity(intent)
         }
 
         databaseRef.child("users").child(uid!!).addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
+                if (this@SettingActivity.isFinishing)
+                    return;
                 setting_nickname_input.setText(snapshot.child("nickname").value as String?)
                 setting_height_input.setText(snapshot.child("height").value.toString())
                 setting_weight_input.setText(snapshot.child("weight").value.toString())
@@ -65,7 +69,6 @@ class SettingActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        var databaseRef : DatabaseReference = database!!.reference
         if (requestCode == PICK_IMAGE_FROM_ALBUM)
             if(resultCode == Activity.RESULT_OK) {
                 // This is path to the selected image
@@ -75,5 +78,33 @@ class SettingActivity : AppCompatActivity() {
                 // exit the ProfileSettingActivity if you leave the album without selecting it
                 finish()
             }
+    }
+
+    fun profileUpload() {
+        // Make file name
+        var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        var imageFileName = "IMAGE_" + timestamp + "_.png"
+
+        var storageRef = storage?.reference?.child("images")?.child(imageFileName)
+        var databaseRef : DatabaseReference = database!!.reference
+
+        var profileUri = Uri.parse(photoUri.toString())
+
+        // File upload
+        storageRef?.putFile(profileUri!!)?.continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
+            return@continueWithTask storageRef.downloadUrl
+        }?.addOnSuccessListener { uri ->
+            var nickname = setting_nickname_input.text.toString()
+            var height = Integer.parseInt(setting_height_input.text.toString())
+            var weight = Integer.parseInt(setting_weight_input.text.toString())
+
+            databaseRef.child("users").child(uid!!).child("nickname").setValue(nickname)
+            databaseRef.child("users").child(uid!!).child("height").setValue(height)
+            databaseRef.child("users").child(uid!!).child("weight").setValue(weight)
+            databaseRef.child("users").child(uid!!).child("userProfile").setValue(uri.toString())
+
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
     }
 }
